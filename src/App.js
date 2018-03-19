a15dc3016ed85aac8963147302439169af5b4fb2
soap51 <soap51@outlook.com>
2018-03-20 00:49:49 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Route } from 'react-router-dom';
+import HomePage from './components/page/HomePage';
+import LoginPage from './components/page/LoginPage';
+
+const App = () => {
+  return(<div>
+  <Route exact path="/" component={HomePage} />
+  <Route exact path="/login" component={LoginPage} />
+  </div>)}
+
+
+
+export default App;

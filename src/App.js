596d7244d9261a5e431068e52e0313e3abeaeed7
soap51 <soap51@outlook.com>
2018-03-20 01:00:37 +0700

    Add semantic UI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './components/page/HomePage';
 import LoginPage from './components/page/LoginPage';
 
 const App = () => {
-  return(<div>
+  return(<div className="ui container">
   <Route exact path="/" component={HomePage} />
   <Route exact path="/login" component={LoginPage} />
   </div>)}
